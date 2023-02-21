ea8f3aae7c1739e62c8b1968a59160deb7447679
Kooker <Joshua.M-CTR.Kooker@faa.gov>
2023-02-21 10:13:42 -0500

    Fourth Commit

---
+++ src/main/java/tgf/App.java
@@ -0,0 +1,30 @@
+package tgf;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class App{
+
+
+
+
+    public static void main(String[] args)
+    {
+        //Launches App
+        System.out.println("Launch App");
+        MainFrame mf = new MainFrame();
+        mf.initialize();
+
+
+
+
+
+    
+    
+    }
+
+}
